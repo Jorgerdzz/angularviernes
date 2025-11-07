ec89b03a7dc3acf46e38219884996b4177604d82
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-11-07 14:12:14 +0100

    Consumiendo servicio Api Personas de tres formas distintas (http, axios, fetch)

--- src/app/services/service.persona.ts
+++ src/app/services/service.persona.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "../../environments/environment.development";
+import axios from "axios";
 
 @Injectable()
 export class ServicePersona{
@@ -13,10 +14,17 @@ export class ServicePersona{
         let url = environment.urlApiPersonas;
         let request = "api/personas";
         let promise = new Promise((resolve)=>{
-            this._http.get(url + request).subscribe(response=>{
-                resolve(response);
-            })
+            // this._http.get(url + request).subscribe(response=>{
+            //     resolve(response);
+            // }) CON HTTP
+            // axios.get(url + request).then(response=>{
+            //     resolve(response.data)
+            // }) CON AXIOS
+            fetch(url + request).then(response=>{
+                return response.json()
+            }).then(data => resolve(data))
         })
+        
         return promise;
     }
 
